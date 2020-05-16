385a19f26ef01f7253e21c2571e70f9cef379351
Clement <ziwei.liu@cca.edu>
2020-05-16 13:42:12 -0700

    Updates

---
+++ precache-manifest.6a7d37487836449a5f39a59518ca6207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e40afc61727b8a380755f4ff6d18f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0953a64d723f920f7a20",
+    "url": "/static/css/main.9c0af439.chunk.css"
+  },
+  {
+    "revision": "a0734f469f3e8912627a",
+    "url": "/static/js/2.986764d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.986764d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0953a64d723f920f7a20",
+    "url": "/static/js/main.9c79007a.chunk.js"
+  },
+  {
+    "revision": "26a46451aecdaf212797",
+    "url": "/static/js/runtime-main.7e69e14d.js"
+  }
+]);
