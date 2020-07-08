81872bc3280fe36a0bd888e571c1cf8875c10d53
Clement <ziwei.liu@cca.edu>
2020-07-08 11:28:11 -0700

    change to hashrouter browser base not working

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import PageContent from "PageContent";
 import Home from "Home";
 import NotFound from "NotFound";
 import { QueriesProvider } from "contexts/ImageList.context";
-// import { purple } from "@material-ui/core/colors";
 
 function App() {
   console.log("app running");
@@ -15,12 +14,8 @@ function App() {
     <div className="App">
       <QueriesProvider>
         <Switch>
-          <Route
-            exact
-            path="/test-deply/recaptcha"
-            render={() => <PageContent />}
-          />
-          <Route exact path="/test-deply" render={() => <Home />} />
+          <Route exact path="/recaptcha" render={() => <PageContent />} />
+          <Route exact path="/" render={() => <Home />} />
           <Route render={() => <NotFound />} />
         </Switch>
       </QueriesProvider>
